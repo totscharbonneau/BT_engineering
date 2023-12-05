dbc33cf08fe3351826ebd988bd268369ad1d13a8
Thomas Charbonneau <thomas.charbonneau16@gmail.com>
2023-12-05 15:23:31 -0500

    changed from 20 cm to 15 cm

--- control/state_actions.py
+++ control/state_actions.py
@@ -17,7 +17,7 @@ class StateActions:
         self._stateActions.lineFollowerState = doLineFollowerStateAction(self, lineFollowerState=self._stateActions.lineFollowerState)
         distance = self._api.ultrasonicAvoidance.get_distance()
         print(distance)
-        if((distance < 20) & (distance >= 0)):
+        if((distance < 15) & (distance >= 0)):
             obstacle = True
         else:
             obstacle = False
